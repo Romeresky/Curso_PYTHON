9f21914fc738f326b1e75cbb872ab2d8c62e3f41
Romeresky <estebanromeropasache@gmail.com>
2022-12-27 03:19:34 -0300

    Primeros fundamentos

---
+++ fundamentos/variables_tipos_de_datos_y_metodos_de_entrada.py
@@ -0,0 +1,26 @@
+#print(8+8)                       #Metodo de salida
+#input('Digita una letra')        #Metodo de entrada
+
+
+#numero1 = 9
+#numero2 = 8
+
+#resultado = numero1+numero2
+
+#print(resultado)
+
+
+#int -1 2 8 -67
+#float 1.5
+#str '' ""
+#bool True o False 
+
+#numero1 = float(input('Ingresa un número'))
+#numero2 = float(input('Ingresa un número'))
+#cadena = str('Hola!')
+
+#resultado = numero1+numero2
+#print(resultado)
+
+
+#print(8==8)
