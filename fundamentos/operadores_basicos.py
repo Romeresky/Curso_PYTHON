eeb9da5af1392f34d6b7680f8253f4a63bc23b8d
Romeresky <estebanromeropasache@gmail.com>
2023-03-02 02:12:22 -0300

    Primera actualización del curso

---
+++ fundamentos/operadores_basicos.py
@@ -0,0 +1,33 @@
+"""
+OPERADORES ARITMETICOS
+"""
+
+# numero1 = float(input('Ingresa un número'))
+# numero2 = float(input('Ingresa un número'))
+
+# print(f'Suma {numero1 + numero2}')
+# print(f'Resta {numero1 - numero2}')
+# print(f'Multiplicación {numero1 * numero2}')
+# print(f'División {numero1 / numero2}')
+# print("Módulo ", (numero1 % numero2))
+# print("Potencia ", (numero1 ** numero2))
+# print("División exacta ", (numero1 // numero2))
+
+"""
+OPERADORES RELACIONALES
+"""
+
+# print(" > ", numero1 > numero2)
+# print(" < ", numero1 < numero2)
+# print(" >= ", numero1 >= numero2)
+# print(" <= ", numero1 <= numero2)
+# print(" == ", numero1 == numero2)
+# print(" != ", numero1 != numero2)  #Diferente
+
+"""
+Operadores de asignación
+"""
+numero3 = 8
+numero3 += 1 
+print(numero3)
+
